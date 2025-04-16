574d8b5c8d3e16e3f6f3d6838f2a285494a4c98c
Santiago <56666653+Gann4Life@users.noreply.github.com>
2025-04-16 02:54:10 +0000

    feat(game-manager): run game using a game manager rather than spitting into main.js

---
+++ src/gameManager.js
@@ -0,0 +1,52 @@
+import { Application } from "pixi.js";
+import { CreaturePlayer } from "./entities/creature/creaturePlayer";
+import { CreatureBot } from "./entities/creature/creatureBot";
+
+export class GameManager {
+    constructor(){
+        this.gameEntities = [];
+        this.app = new Application();
+        this.setupApp().then(r => this.onAppInitialized());
+    }
+
+    async setupApp() {
+        await this.app.init({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+        this.app.canvas.style.position = "absolute";
+        document.body.appendChild(this.app.canvas);
+    }
+
+    onAppInitialized() {
+        this.setupAllEntities();
+    }
+
+    setupAllEntities(){
+        this.createPlayer();
+        this.createBots();
+        this.createFood();
+
+        // Adds created entities to the stage.
+        for(let i = 0; i < this.gameEntities.length; i++){
+            const entity = this.gameEntities[i];
+            this.app.stage.addChild(entity.sprite);
+        }
+    }
+
+    createPlayer(){
+        let player = new CreaturePlayer(this.app);
+        this.gameEntities.push(player);
+    }
+
+    createBots(){
+        for(let i = 0; i < 4; i++){
+            let bot = new CreatureBot(this.app, this.gameEntities);
+            this.gameEntities.push(bot);
+        }
+    }
+
+    createFood(){
+
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,3 @@
-import { Application, Graphics } from 'pixi.js';
-import { ExampleEntity } from './entities/exampleEntiy.js';
-import {Creature} from "./entities/creature/creature";
-import {CreaturePlayer} from "./entities/creature/creaturePlayer";
-import {CreatureBot} from "./entities/creature/creatureBot";
+import {GameManager} from "./gameManager";
 
-(async() => {
-    const app = new Application();
-
-    await app.init({
-        width: window.innerWidth,
-        height: window.innerHeight
-    });
-
-    app.canvas.style.position = 'absolute';
-
-    const gameEntities = [];
-
-    let player = new CreaturePlayer(app);
-    gameEntities.push(player);
-
-    for(let i = 0; i < 4; i++){
-        let bot = new CreatureBot(app, gameEntities);
-        gameEntities.push(bot);
-    }
-
-    for(let i = 0; i < gameEntities.length; i++){
-        const entity = gameEntities[i];
-        app.stage.addChild(entity.sprite);
-    }
-
-    document.body.appendChild(app.canvas);
-})();
+const gameManager = new GameManager();
